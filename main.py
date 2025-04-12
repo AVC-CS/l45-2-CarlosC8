7c65d1fecb3edb08846447f09fb2b48c1322c8c9
CarlosC8 <ccontreras32@students.avc.edu>
2025-04-11 19:47:49 -0700

    Completed lab, no errors

--- main.py
+++ main.py
@@ -10,6 +10,20 @@ def main():
     ########################################
     """
 
+    numbers = []
+    total = 0
+    i = 0
+
+    while total < 100:
+        numbers.append(random.randint(1,10))
+        total = total + numbers[i]
+        i = i + 1
+
+    idx = len(numbers)
+    if total > 100:
+        total = total - numbers[idx - 1]
+        
+
     print(f'The random values are {numbers}')
     print(f'The total is {total}')
 
